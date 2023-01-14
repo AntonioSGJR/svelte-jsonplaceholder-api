d18a484819f42ef709064bf1529db9654b1b5457
matheusinit <matheus.oliveira.s@protonmail.com>
2023-01-14 15:18:29 -0300

    add home page

---
+++ src/pages/Home.svelte
@@ -0,0 +1,15 @@
+<script>
+  import {onMount} from 'svelte'  
+
+
+  onMount(() => {
+    fetch("https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Bourbon")
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+    }).catch(error => {
+      console.log(error);
+      return [];
+    });
+  })
+</script>
