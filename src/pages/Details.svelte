47ab063e6c3c3fcb16d56943da4e97d630312266
Felipe Rocha <77278366+FelipeSousaRocha@users.noreply.github.com>
2023-01-15 14:54:44 +0000

    add styles

--- src/pages/Details.svelte
+++ src/pages/Details.svelte
@@ -1,58 +1,108 @@
 <script>
-  export let id;
-  import { onMount } from 'svelte'  
-  import { writable } from 'svelte/store';
+  export let id
+  import { onMount } from 'svelte'
+  import { writable } from 'svelte/store'
 
-  let albumState = writable({});
-  let photosState = writable([]);
-  let album;
+  let albumState = writable({})
+  let photosState = writable([])
+  let album
 
-  albumState.subscribe(value => album = value);
+  albumState.subscribe(value => (album = value))
 
   onMount(() => {
     fetch(`https://jsonplaceholder.typicode.com/albums/${id}`)
-    .then(response => response.json())
-    .then(albums => {
-      albumState.set(albums);
-    })
-    .catch(error => {
-      console.log(error);
-      return [];
-    });
+      .then(response => response.json())
+      .then(albums => {
+        albumState.set(albums)
+      })
+      .catch(error => {
+        console.log(error)
+        return []
+      })
 
     fetch(`https://jsonplaceholder.typicode.com/albums/${id}/photos`)
-    .then(response => response.json())
-    .then(photos => {
-      photosState.set(photos);
-    })
-    .catch(error => {
-      console.log(error);
-      return [];
-    });
-  });
+      .then(response => response.json())
+      .then(photos => {
+        photosState.set(photos)
+      })
+      .catch(error => {
+        console.log(error)
+        return []
+      })
+  })
 </script>
 
 <main>
+  <h2>Album</h2>
+  <div class="info">
     <h1>{album.title}</h1>
-    <p>Album: {album.id}</p>
-    <p>Usuario: {album.userId}</p>
-    <h2>Photos</h2>
+    <p>Albúm: {album.id}</p>
+    <p>Usuário: {album.userId}</p>
+  </div>
+  <div>
+    <h2>Fotos</h2>
     <div class="photos">
       {#each $photosState as photo}
-        <img src="{photo.thumbnailUrl}" alt="Album photo" class="card">
+        <img src={photo.thumbnailUrl} alt="Album photo" class="card" />
       {/each}
     </div>
+  </div>
 </main>
 
 <style>
-main {
-  padding: 0rem 1rem 1rem 1rem;
-}
+  main {
+    padding: 1em;
+    background-color: rgb(0, 0, 0);
+    padding: 10px;
+    transition: all 0.3s ease-out;
+  }
 
-.photos {
-  display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(9rem, 1fr));
-  gap: 1rem;
-}
+  .info {
+    border-radius: 10px;
+    box-shadow: 0px 0px 0px 4px rgba(44, 0, 114, 0.8);
+    padding: 30px;
+    margin: 20px;
+    transition: all 0.3s ease-out;
+  }
 
+  h1 {
+    color: white;
+  }
+
+  h2 {
+    color: white;
+    margin-left: 18px;
+  }
+
+  p {
+    color: white;
+  }
+
+  h2 {
+    color: white;
+  }
+
+  .photos {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(9rem, 1fr));
+    gap: 1rem;
+    border-radius: 10px;
+    box-shadow: 0px 0px 0px 4px rgba(44, 0, 114, 0.8);
+    padding: 30px;
+    margin: 20px;
+    transition: all 0.3s ease-out;
+  }
+
+  .photos,
+  img {
+    border-radius: 6px;
+  }
+
+  main::first-letter {
+    text-transform: uppercase;
+  }
+
+  .info::first-letter {
+    text-transform: uppercase;
+  }
 </style>
--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -1,23 +1,24 @@
 <script>
-  import { onMount } from 'svelte'  
-  import { writable } from 'svelte/store';
+  import { onMount } from 'svelte'
+  import { writable } from 'svelte/store'
 
   let albumsState = writable([])
 
   onMount(() => {
-    fetch("https://jsonplaceholder.typicode.com/albums/")
-    .then(response => response.json())
-    .then(albums => {
-      albumsState.set(albums)
-    }).catch(error => {
-      console.log(error);
-      return [];
-    });
+    fetch('https://jsonplaceholder.typicode.com/albums/')
+      .then(response => response.json())
+      .then(albums => {
+        albumsState.set(albums)
+      })
+      .catch(error => {
+        console.log(error)
+        return []
+      })
   })
 </script>
 
 <div class="home">
-  <h1>Albums</h1>
+  <h1>Albuns</h1>
 
   <div class="card-wrapper">
     {#each $albumsState as album}
@@ -27,29 +28,42 @@
 </div>
 
 <style>
+  @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap');
+
   .home {
     padding: 1em;
+    background-color: rgb(0, 0, 0);
   }
 
   .card-wrapper {
     display: flex;
     flex-wrap: wrap;
-
     justify-content: space-between;
     row-gap: 1em;
   }
 
   .card {
-    border: 1px solid #ccc;
-    width: 20%;
-    height: 200px;
+    border-radius: 10px;
+    box-shadow: 0px 0px 0px 4px rgba(44, 0, 114, 0.8);
+    padding: 30px;
+    margin: 20px;
+    width: 200px;
+    transition: all 0.3s ease-out;
+  }
 
-    display: flex;
-    justify-content: center;
-    align-items: center;
+  .card:hover {
+    transform: translateY(-5px);
+    cursor: pointer;
+    color: blueviolet;
+    box-shadow: 0px 0px 0px 4px rgba(195, 0, 255, 0.8);
+  }
 
-    color: #444;
+  h1 {
+    color: white;
+    margin-left: 18px;
+  }
 
-    border-radius: 6px;
+  .card::first-letter {
+    text-transform: uppercase;
   }
 </style>
